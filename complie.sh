b158a4ea4fce8d09a20d0fc84b1bf798ae4cb394
duzhengbin <847479962@qq.com>
2018-09-14 10:04:46 +0800

    由于新增加代码使用到context，低版本go语言不支持，故删除该环境下的编译脚本，只保留一个编译脚本，只使用go1.9.4版本编译

--- complie.sh
+++ complie.sh
@@ -1,9 +1,1 @@
-#! /bin/bash
-# export GOPATH=/go
-# export GOROOT=/go  ~/.bashrc文件 默认的GOROOT不用改
-echo "begin..."
-export GOPATH=/mnt/hgfs/mygit/mygit/
-export GOROOT=/usr/lib/go-1.6
-cd mygit/
-go build .
-echo "end..."
+echo "begin...";export GOPATH=`pwd`/mygit;cd mygit/;go build .;echo "end...";
--- complie_16_04_4.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# export GOPATH=/go
-# export GOROOT=/go  ~/.bashrc文件 默认的GOROOT不用改
-echo "begin..."
-export GOPATH=/mnt/hgfs/mygitrepo/mygit/mygit/
-#export GOROOT=/usr/lib/go-1.6
-cd mygit/
-go build .
-echo "end..."
